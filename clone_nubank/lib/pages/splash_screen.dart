fd6f849c71ae904277f1997ad4dd46bc330ecec5
Andrei Silveira Morais <andrei.morais@outlook.com>
2022-01-12 11:42:09 -0300

    updates

---
+++ clone_nubank/lib/pages/splash_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      color: Colors.purple.shade700,
+      child: Center(
+        child: Image.asset('assets/nubank_splash_logo.png'),
+      ),
+    );
+  }
+}
