7f7955ed7dc19456cff85b030f91db6a753c2a84
Andrei Silveira Morais <andrei.morais@outlook.com>
2022-01-12 16:04:50 -0300

    Update login_page.dart

--- clone_nubank/lib/pages/login_page.dart
+++ clone_nubank/lib/pages/login_page.dart
@@ -10,19 +10,37 @@ class LoginPage extends StatelessWidget {
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
         color: Colors.purple.shade700,
-        child: Center(
-          child: Image.asset(
-            'assets/nubank_splash.png',
-            height: 100,
-          ),
+        child: Column(
+          children: [
+            const Spacer(),
+            Image.asset(
+              'assets/nubank_splash.png',
+              height: 100,
+            ),
+            const Spacer(),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 30),
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                ),
+                onPressed: () {},
+                child: const Padding(
+                  padding: EdgeInsets.only(
+                      left: 130, right: 130, top: 12, bottom: 12),
+                  child: Text(
+                    'Entrar',
+                    style: TextStyle(fontSize: 17),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        label: Text(
-          'Entrar',
-        ),
-        extendedPadding: EdgeInsets.all(160),
-        onPressed: () {},
       ),
     );
   }
