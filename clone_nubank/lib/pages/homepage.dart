7d95ca11e104f3f6d77da5464b19968c65e9c7b6
Andrei Silveira Morais <andrei.morais@outlook.com>
2022-01-14 01:58:25 -0300

    updates

--- clone_nubank/lib/pages/homepage.dart
+++ clone_nubank/lib/pages/homepage.dart
@@ -89,7 +89,7 @@ class _HomePageState extends State<HomePage> {
                 CustomColumn(
                   text: 'Área Pix',
                   onPressed: () {},
-                  icon: Icons.ac_unit,
+                  icon: Icons.pix,
                 )
               ],
             )
