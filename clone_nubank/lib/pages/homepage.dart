2d947cbdf8dcbb6e7480920599ccd48b66c5c9c2
Andrei Silveira Morais <andrei.morais@outlook.com>
2022-01-19 14:30:23 -0300

    updates bottom sheet

--- clone_nubank/lib/pages/homepage.dart
+++ clone_nubank/lib/pages/homepage.dart
@@ -141,64 +141,9 @@ class _HomePageState extends State<HomePage> {
                 CustomRoundedButton(
                   text: 'Pagar',
                   onPressed: () {
-                    widget.bottonSheet.showBottomSheet(
+                    widget.bottonSheet.pagarBottomSheet(
                       context: context,
                       height: MediaQuery.of(context).size.height * .85,
-                      children: [
-                        Row(
-                          children: [
-                            IconButton(
-                              icon: const Icon(Icons.close),
-                              onPressed: () => Navigator.of(context).pop(),
-                            ),
-                          ],
-                        ),
-                        CustomListTile(
-                          leading: const Icon(Icons.local_parking),
-                          title: 'Pagar com Pix',
-                          subtitle: 'Leia um QR Code ou cole o código',
-                          trailing: const Icon(
-                            Icons.arrow_forward_ios,
-                            size: 20,
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 50),
-                          child: CustomListTile(
-                            leading: const Icon(Icons.credit_card_rounded),
-                            title: 'Pagar fatura do cartão',
-                            subtitle:
-                                'Libere o limite do seu cartão de crédito',
-                            trailing: const Icon(
-                              Icons.arrow_forward_ios,
-                              size: 20,
-                            ),
-                          ),
-                        ),
-                        CustomListTile(
-                          leading: const Icon(MdiIcons.barcode),
-                          title: 'Pagar boleto com saldo da Conta',
-                          subtitle:
-                              'use o saldo da sua conta do Nubank para pagar seus boletos',
-                          trailing: const Icon(
-                            Icons.arrow_forward_ios,
-                            size: 20,
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 50),
-                          child: CustomListTile(
-                            leading: const Icon(MdiIcons.barcode),
-                            title: 'Pagar boleto com cartão de crédito',
-                            subtitle:
-                                'Use o limite do seu cartão para pagar suas contas em até 12x',
-                            trailing: const Icon(
-                              Icons.arrow_forward_ios,
-                              size: 20,
-                            ),
-                          ),
-                        ),
-                      ],
                     );
                   },
                   icon: MdiIcons.barcode,
@@ -206,69 +151,20 @@ class _HomePageState extends State<HomePage> {
                 CustomRoundedButton(
                   text: 'Transferir',
                   onPressed: () {
-                    widget.control.text = 'R\$ 0,00';
-                    widget.bottonSheet.showBottomSheet(
-                      height: MediaQuery.of(context).size.height * .90,
-                      context: context,
-                      children: [
-                        Row(
-                          children: [
-                            IconButton(
-                              icon: Icon(Icons.close),
-                              onPressed: () => Navigator.of(context).pop(),
-                            ),
-                            Spacer(),
-                            IconButton(
-                              icon: Icon(MdiIcons.qrcode),
-                              onPressed: () {},
-                            )
-                          ],
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(20.0),
-                          child: Text(
-                            'Qual é o valor da transferência?',
-                            style: Theme.of(context).textTheme.headline3,
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 20),
-                          child: Text(
-                            'Saldo disponível em conta',
-                            style: TextStyle(
-                                fontSize: 20, color: Colors.grey.shade600),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 20),
-                          child: Text(
-                            saldo,
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 20),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(
-                              right: 20, left: 20, top: 40),
-                          child: TextField(
-                            //nao está formatado corretamente, editar isso no futuro
-                            controller: widget.control,
-                            keyboardType: TextInputType.number,
-                            inputFormatters: [
-                              FilteringTextInputFormatter.digitsOnly,
-                              FilteringTextInputFormatter.singleLineFormatter,
-                            ],
-                            style: Theme.of(context).textTheme.headline3,
-                          ),
-                        ),
-                      ],
-                    );
+                    widget.bottonSheet.tranferirBottomSheet(
+                        height: MediaQuery.of(context).size.height * .90,
+                        context: context,
+                        saldo: saldo);
                   },
                   icon: MdiIcons.cashFast,
                 ),
                 CustomRoundedButton(
                   text: 'Depositar',
-                  onPressed: () {},
+                  onPressed: () {
+                    widget.bottonSheet.depositarBottomSheet(
+                        context: context,
+                        height: MediaQuery.of(context).size.height * .90);
+                  },
                   icon: MdiIcons.cashPlus,
                 ),
                 CustomRoundedButton(
--- clone_nubank/lib/widgets/custom_bottom_sheet.dart
+++ clone_nubank/lib/widgets/custom_bottom_sheet.dart
@@ -1,6 +1,10 @@
+import 'package:clone_nubank/widgets/custom_listtile.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class CustomBottonSheet {
+  TextEditingController control = TextEditingController(text: 'R\$ 0,00');
   showBottomSheet(
       {required BuildContext context,
       required List<Widget> children,
@@ -15,9 +19,240 @@ class CustomBottonSheet {
             SizedBox(
               height: height,
               width: width,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: children,
+              child: SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: children,
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void pagarBottomSheet(
+      {required BuildContext context, double? height, double? width}) {
+    showModalBottomSheet(
+      isScrollControlled: true,
+      context: context,
+      builder: (context) {
+        return Wrap(
+          children: [
+            SizedBox(
+              height: height,
+              width: width,
+              child: SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.close),
+                          onPressed: () => Navigator.of(context).pop(),
+                        ),
+                      ],
+                    ),
+                    CustomListTile(
+                      leading: const Icon(Icons.local_parking),
+                      title: 'Pagar com Pix',
+                      subtitle: 'Leia um QR Code ou cole o código',
+                      trailing: const Icon(
+                        Icons.arrow_forward_ios,
+                        size: 20,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 50),
+                      child: CustomListTile(
+                        leading: const Icon(Icons.credit_card_rounded),
+                        title: 'Pagar fatura do cartão',
+                        subtitle: 'Libere o limite do seu cartão de crédito',
+                        trailing: const Icon(
+                          Icons.arrow_forward_ios,
+                          size: 20,
+                        ),
+                      ),
+                    ),
+                    CustomListTile(
+                      leading: const Icon(MdiIcons.barcode),
+                      title: 'Pagar boleto com saldo da Conta',
+                      subtitle:
+                          'use o saldo da sua conta do Nubank para pagar seus boletos',
+                      trailing: const Icon(
+                        Icons.arrow_forward_ios,
+                        size: 20,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 50),
+                      child: CustomListTile(
+                        leading: const Icon(MdiIcons.barcode),
+                        title: 'Pagar boleto com cartão de crédito',
+                        subtitle:
+                            'Use o limite do seu cartão para pagar suas contas em até 12x',
+                        trailing: const Icon(
+                          Icons.arrow_forward_ios,
+                          size: 20,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void tranferirBottomSheet(
+      {required BuildContext context,
+      double? height,
+      double? width,
+      required String saldo}) {
+    showModalBottomSheet(
+      isScrollControlled: true,
+      context: context,
+      builder: (context) {
+        return Wrap(
+          children: [
+            SizedBox(
+              height: height,
+              width: width,
+              child: SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.close),
+                          onPressed: () => Navigator.of(context).pop(),
+                        ),
+                        const Spacer(),
+                        IconButton(
+                          icon: const Icon(MdiIcons.qrcode),
+                          onPressed: () {},
+                        )
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Text(
+                        'Qual é o valor da transferência?',
+                        style: Theme.of(context).textTheme.headline3,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Text(
+                        'Saldo disponível em conta',
+                        style: TextStyle(
+                            fontSize: 20, color: Colors.grey.shade600),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Text(
+                        saldo,
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                    ),
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(right: 20, left: 20, top: 40),
+                      child: TextField(
+                        //nao está formatado corretamente, editar isso no futuro
+                        controller: control,
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly,
+                          FilteringTextInputFormatter.singleLineFormatter,
+                        ],
+                        style: Theme.of(context).textTheme.headline3,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void depositarBottomSheet(
+      {required BuildContext context, double? height, double? width}) {
+    showModalBottomSheet(
+      isScrollControlled: true,
+      context: context,
+      builder: (context) {
+        return Wrap(
+          children: [
+            SizedBox(
+              height: height,
+              width: width,
+              child: SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.close),
+                          onPressed: () => Navigator.of(context).pop(),
+                        ),
+                      ],
+                    ),
+                    Text(
+                      'Como você quer depositar na sua conta do Nubank?',
+                      style: Theme.of(context).textTheme.headline3,
+                    ),
+                    CustomListTile(
+                      leading: const Padding(
+                        padding: EdgeInsets.only(top: 25),
+                        child: Icon(Icons.local_parking),
+                      ),
+                      title: 'Receber na hora em qualquer dia, sem custo',
+                      subtitle:
+                          'Gere um código Pix, com ou sem chave, para copiar e colar no seu banco.',
+                      trailing: const Icon(Icons.arrow_forward_ios, size: 20),
+                    ),
+                    CustomListTile(
+                      leading: const Icon(MdiIcons.barcode),
+                      title: 'Receber em até 3 dias, sem custo',
+                      subtitle: 'Gere um boleto de depósito.',
+                      trailing: const Icon(Icons.arrow_forward_ios, size: 20),
+                    ),
+                    CustomListTile(
+                      leading: const Icon(MdiIcons.cashPlus),
+                      title: 'Copiar dados da conta',
+                      subtitle:
+                          'Usar os dados da sua conta para transferir usando Pix, TED e DOC',
+                      trailing: const Icon(Icons.arrow_forward_ios, size: 20),
+                    ),
+                    CustomListTile(
+                      leading: const Padding(
+                        padding: EdgeInsets.only(top: 25),
+                        child: Icon(MdiIcons.cashMultiple),
+                      ),
+                      title: 'Receber salário, sem custo',
+                      subtitle:
+                          'O salário cai no mesmo dia em que é depositado onde você tem conta.',
+                      trailing: const Icon(Icons.arrow_forward_ios, size: 20),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
@@ -26,5 +261,3 @@ class CustomBottonSheet {
     );
   }
 }
-
-
